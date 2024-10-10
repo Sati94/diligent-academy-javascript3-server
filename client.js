27179889054928720e4b8679c908c75ca88691e3
Ati <nagyonkellvalamihez@gmail.com>
2024-10-10 19:38:21 +0200

    implement CRUD fetches

--- client.js
+++ client.js
@@ -1,7 +1,50 @@
 const prompt = require('prompt-sync')()
 
-fetch(`http://localhost:3000/users/1`, {
-    method: 'DELETE'
+
+
+/*fetch(`http://localhost:3000/todos`, {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ title: 'Something' })
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+
+fetch(`http://localhost:3000/todos`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+fetch(`http://localhost:3000/todos/2`, {
+    method: 'PUT',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ title: 'New Title' })
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    })*/
+
+fetch(`http://localhost:3000/todos/2`, {
+    method: 'Delete'
+
 })
     .then(response => response.json())
     .then(data => {
