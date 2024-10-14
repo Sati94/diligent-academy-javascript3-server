329bdccd32b86908f778687e9ffa236f5e404327
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2024-10-14 13:14:26 +0200

    refactor

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 const prompt = require('prompt-sync')()
 const BASE_URL = 'http://localhost:3000/todos';
 
+const getInput = (message) => {
+  return prompt(`${message}: `)
+}
+
 const getTodos = async () => {
     try {
         const response = await fetch(BASE_URL)
@@ -13,14 +17,14 @@ const getTodos = async () => {
 }
 
 const postTodo = async () => {
-    const newTodoTitle = prompt('Enter todo title: ')
+    const newTodoTitle = getInput('Enter todo title');
     try {
         const response = await fetch(BASE_URL, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
             },
-            body: JSON.stringify({ title: newTodoTitle })
+            body: JSON.stringify({title: newTodoTitle})
         })
         const newTodo = await response.json();
         console.log('New todo created:', newTodo);
@@ -29,11 +33,10 @@ const postTodo = async () => {
         console.error('Error posting todos:' + error);
     }
 }
-//postTodo();
 
 const putTodo = async () => {
-    const updatedId = prompt('Enter the ID of the todo to update: ');
-    const updatedTitle = prompt('Enter the new title: ');
+    const updatedId = getInput('Enter the ID of the todo to update');
+    const updatedTitle = getInput('Enter the new title');
 
     try {
         const response = await fetch(`${BASE_URL}/${updatedId}`, {
@@ -41,13 +44,12 @@ const putTodo = async () => {
             headers: {
                 'Content-type': 'application/json'
             },
-            body: JSON.stringify({ title: updatedTitle })
+            body: JSON.stringify({title: updatedTitle})
         })
         if (response.ok) {
             const updatedTodo = await response.json();
             console.log('Todo updated : ' + updatedTodo.title);
-        }
-        else {
+        } else {
             console.error('Error updating todo: ' + await response.json());
         }
 
@@ -55,17 +57,16 @@ const putTodo = async () => {
         console.error('Error updating todos:' + error);
     }
 }
-//putTodo();
+
 const deleteTodo = async () => {
-    const deleteId = prompt('Enter the ID of the todo to delete: ');
+    const deleteId = getInput('Enter the ID of the todo to delete');
     try {
         const response = await fetch(`${BASE_URL}/${deleteId}`, {
             method: "DELETE"
         })
         if (response.ok) {
             console.log('Todo deleted successfully.')
-        }
-        else {
+        } else {
             console.error('Error deleting todo:' + await response.json());
         }
 
